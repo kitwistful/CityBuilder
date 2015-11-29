48e3c785185787120cfb6402a408badbf2daafc1
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 15:01:04 -0500

    filled in dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,5 +9,19 @@
     define("CURRENT_PAGE", "dashboard.php");
     include "header.php";
 ?>
+    <article>
+        <header>Your Cities</header>
+        <content>
+            <ul>
+                <li><a class = "dashboard_city_link" href = "#">First City</a></li>
+                <li><a class = "dashboard_city_link" href = "#">Someburb</a></li>
+                <li><a class = "dashboard_city_link" href = "#">Polispolis</a></li>
+            </ul>
+        </content>
+    </article>
+    <article>
+        <div id = "dashboard_city_content"></div>
+    </article>
+    
 </body>
 </html>
