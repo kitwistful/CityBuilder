bee0843113cdc2f0aef6b43121221207c6402ae7
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 14:56:01 -0500

    filled in login, added style

--- login.php
+++ login.php
@@ -9,5 +9,13 @@
     define("CURRENT_PAGE", "login.php");
     include "header.php";
 ?>
+    <article>
+        <header>Login</header>
+        <form id = "login_form">
+            <input id = "username" type = "text"></input>
+            <input id = "password" type = "password"></input>
+            <button id = "login_button">Go</input>
+        </form>
+    </article>
 </body>
 </html>
