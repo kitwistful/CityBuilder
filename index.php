035eaea8e0f8c13b21c4463076071760bda89a0b
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 14:48:14 -0500

    initialized homepage.

--- index.php
+++ index.php
@@ -9,5 +9,12 @@
     define("CURRENT_PAGE", "index.php");
     include "header.php";
 ?>
+
+    <article>
+        <header>Welcome to City Builder!</header>
+        <content>
+            To get started, please <a href = "#">create a player account</a> or <a href = "login.php">login to an existing one</a>. You can also <a href = "recover_account.php">recover an account</a> if you have a code. Have fun!
+        </content>
+    </article>
 </body>
 </html>
