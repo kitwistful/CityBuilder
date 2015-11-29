67a1cc584755e7f7cccf5a842735fa4a35aa5760
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 16:48:31 -0500

    setup form validation

--- create_account.php
+++ create_account.php
@@ -9,22 +9,40 @@
     define("CURRENT_PAGE", "create_account.php");
     include "header.php";
 ?>
+<?php
+    
+    
+    $signup_message = "";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        define("SIGNUP_SHOW_MESSAGE", true);
+        
+        $signup_message = "test";
+    } else{
+        define("SIGNUP_SHOW_MESSAGE", false);
+    }
+?>
 
     <article>
         <header>Sign up for City Builder</header>
-        <content>
-            To continue, choose your username and password.
-        </content>
-        <form id = "create_account_form">
+<?php
+    if(SIGNUP_SHOW_MESSAGE)
+    {
+        echo "<content><div class = 'signup_error'><header>Information</header><content>$signup_message</content></div></content>";
+    } else {
+        echo "<content>To continue, choose your username and password.</content>";
+    }
+?>
+        <form id = "create_account_form" method = "POST" action = "create_account.php">
             <label>Username:</label>
-            <input id = "create_account_username" type = "text"></input>
+            <input id = "create_account_username" name = "username" type = "text"></input>
             <label>Password:</label>
-            <input id = "create_account_password" type = "password"></input>
+            <input id = "create_account_password" name = "password" type = "password"></input>
             <label>Re-enter your password:</label>
-            <input id = "create_account_password_again" type = "password"></input>
+            <input id = "create_account_password_again" name = "password_again" type = "password"></input>
             <button id = "create_account_confirm">Signup</button>
         </form>
-        <div id = "create_account_message"></div>
     </article>
 </body>
 </html>
