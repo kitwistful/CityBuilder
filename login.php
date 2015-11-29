e3b1685ed6e4bbea12508fbbedf83e3f2fc22b14
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 17:14:48 -0500

    updated login page to have validation

--- login.php
+++ login.php
@@ -6,14 +6,62 @@
 </head>
 <body>
 <?php
+    // header
     define("CURRENT_PAGE", "login.php");
     include "header.php";
+    
+    // message to display
+    $signup_message = null;
+    
+    // form info
+    $username = "";
+
+    // form validation
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // get form info
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        // begin listing errors
+        $signup_message = "";
+        
+        // check username
+        if($username == null)
+        {
+            $signup_message = $signup_message."<li>username is required</li>";
+            $username = "";
+        }
+        
+        // check password
+        if($password == null)
+        {
+            $signup_message = $signup_message."<li>password is required</li>";
+        }
+        
+        // compile list
+        if($signup_message != "")
+        {
+            $signup_message = "<content>Found the following errors:</content><ul>".$signup_message."</ul>";
+        } else {
+            // obviously nothing is wrong
+            $signup_message = "successful";
+        }
+    }
 ?>
     <article>
         <header>Login</header>
-        <form id = "login_form">
-            <input id = "username" type = "text"></input>
-            <input id = "password" type = "password"></input>
+<?php
+    if($signup_message != null)
+    {
+        echo "<content><div class = 'signup_error'><header>Information</header><content>$signup_message</content></div></content>";
+    }
+?>
+        <form id = "login_form" method = "POST" action = "login.php">
+            <label>Username:</label>
+            <input id = "username" type = "text" name = "username" value = <?php echo "\"$username\""?>></input>
+            <label>Password:</label>
+            <input id = "password" type = "password" name = "password"></input>
             <button id = "login_button">Go</input>
         </form>
     </article>
