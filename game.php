55e769399eb3d20564003e3f721280dca0192704
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-29 19:52:49 -0500

    city selection now defaults to currently selected

--- game.php
+++ game.php
@@ -47,30 +47,30 @@
     
 ?>
 <article id = "curr_city">
-<header>'<?php echo $cities[$curr_city] ?>'</header>
-<div id = "game_sector_display">
-    <div id = "game_sector_display_q1" class = <?php echo $sector_display_class[SECTOR_RESIDENTIAL]?> >
-        Residential: <?php echo $sector_blocks[SECTOR_RESIDENTIAL]?>
+    <header>'<?php echo $cities[$curr_city] ?>'</header>
+    <div id = "game_sector_display">
+        <div id = "game_sector_display_q1" class = <?php echo $sector_display_class[SECTOR_RESIDENTIAL]?> >
+            Residential: <?php echo $sector_blocks[SECTOR_RESIDENTIAL]?>
+        </div>
+        <div id = "game_sector_display_q2" class = <?php echo $sector_display_class[SECTOR_EDUCATIONAL]?> >
+            Educational: <?php echo $sector_blocks[SECTOR_EDUCATIONAL]?>
+        </div>
+        <div id = "game_sector_display_q3" class = <?php echo $sector_display_class[SECTOR_BUSINESS]?> >
+            Business: <?php echo $sector_blocks[SECTOR_BUSINESS]?>
+        </div>
+        <div id = "game_sector_display_q4" class = <?php echo $sector_display_class[SECTOR_RECREATIONAL]?> >
+            Recreational: <?php echo $sector_blocks[SECTOR_RECREATIONAL]?>
+        </div>
     </div>
-    <div id = "game_sector_display_q2" class = <?php echo $sector_display_class[SECTOR_EDUCATIONAL]?> >
-        Educational: <?php echo $sector_blocks[SECTOR_EDUCATIONAL]?>
-    </div>
-    <div id = "game_sector_display_q3" class = <?php echo $sector_display_class[SECTOR_BUSINESS]?> >
-        Business: <?php echo $sector_blocks[SECTOR_BUSINESS]?>
-    </div>
-    <div id = "game_sector_display_q4" class = <?php echo $sector_display_class[SECTOR_RECREATIONAL]?> >
-        Recreational: <?php echo $sector_blocks[SECTOR_RECREATIONAL]?>
-    </div>
-</div>
-<content>
-    <div id = "city_description">What is there to say about this place?</div>
-</content>
-<ul>
-    <li>Blocks:  <?php echo $n_blocks?></li>
-    <li>Unused Blocks: <?php echo $n_blocks_unused?></li>
-    <li>Coins: <?php echo $n_coins?></li>
-    <li>Current Sector: <?php echo $curr_sector?></li>
-</ul>
+    <content>
+        <div id = "city_description">What is there to say about this place?</div>
+    </content>
+    <ul>
+        <li>Blocks:  <?php echo $n_blocks?></li>
+        <li>Unused Blocks: <?php echo $n_blocks_unused?></li>
+        <li>Coins: <?php echo $n_coins?></li>
+        <li>Current Sector: <?php echo $curr_sector?></li>
+    </ul>
 </article>
 
 <article>
@@ -82,8 +82,8 @@
         // print radio button
         echo "<input class = 'radio_input' type = 'radio' name = 'city' value = $k ";
         
-        // preselect the first entry
-        if($k == 0)
+        // preselect the current entry
+        if($k == $curr_city)
             echo "checked";
         
         // print the city name
