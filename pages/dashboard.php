94e7a86b68016929afb47741c20d40d05b474b5b
Kathryn McKay <kathryn.mckay@uoit.net>
2015-12-01 15:50:43 -0500

    fixed bug relating to page rearrange

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -34,7 +34,7 @@
     {
         echo "<article><header>Welcome to City Builder!</header><content>To get started, please <a href = 'create_account.php'>create a player account</a> or <a href = 'login.php'>login to an existing one</a>. <!--You can also <a href = 'recover_account.php'>recover an account</a> if you have a code.--> Have fun!</content></article>";
     } else {
-        include 'game.php';
+        include '../scripts/game.php';
     }
 ?>
     
