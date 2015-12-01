c883823721d6e349d2d39b7f5aebd6e264dee41c
Kathryn McKay <kathryn.mckay@uoit.net>
2015-12-01 15:17:57 -0500

    splitup database creation into several files

--- database.php
+++ database.php
@@ -1,23 +1,4 @@
 <?php
-    $servername = "localhost";
-    $dbname = "citybdb";
-    $username = "root";
-    $password = "";
-    
-    $sql = "CREATE DATABASE $dbname";
-
-    try {
-        $conn = new PDO("mysql:host=$servername;", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $conn->exec($sql);
-        
-        echo "Database created successfully<br />";
-    }   
-    catch (PDOException $e)
-    {
-        echo $sql."<br />".$e->getMessage();
-    }
-    
-    $conn = null;
+    include "database/create_database.php";
+    include "database/init_database.php";
 ?>
