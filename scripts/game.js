a687a71eeba0d1016e4e3151076082af708ba065
Kathryn McKay <kathryn.mckay@uoit.net>
2015-12-03 20:01:58 -0500

    pulled callback function out and data

--- scripts/game.js
+++ scripts/game.js
@@ -7,19 +7,26 @@
 * This file has the functions and such for running the game.
 *
 **/
+
+var CityBuilder_outputdivname = "#testReload";
+
+function CityBuilder_gotResult(text)
+{
+    $(CityBuilder_outputdivname).html("got: "+text+"<br />" );
+}
+
 function gameLoop(name)
 {
+    // data to give database
+    var data = {
+        testmessage: "bread"
+        };
+    
     // about to post
     $(name).html("getting info from database....");
     
     // ask for data
-    $.post("../scripts/game_update.php",
-        {testmessage: "bread"
-        },
-        function(text){
-        $(name).html("got: "+text+"<br />" );
-        },
-        "text");
+    $.post("../scripts/game_update.php",data, CityBuilder_gotResult,"text");
         
     // wait 1 minute and then go again
     setTimeout(function(){
